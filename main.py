fe7cffa8f057d473c22a1b35eb65142f8052e661
thrishapatil <patilthrisha321@gmal.com>
2025-06-23 21:24:24 +0530

    add

--- main.py
+++ main.py
@@ -2,12 +2,20 @@ from flask import Flask,render_template,request,session,redirect,url_for
 from flask_sqlalchemy import SQLAlchemy 
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash,check_password_hash
-from flask_login import login_user,logout_user
+from flask_login import login_user,logout_user,login_manager,LoginManager,login_required,current_user
 
 #my database connection
 local_server=True
 app = Flask(__name__)
 app.secret_key="thrisshapatil"
+
+#this is for getting unique user session
+login_manager=LoginManager(app)
+login_manager.login_view='login'   
+     
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))  
 
 #app.config['SQLALCHEMY_DATABASE_URI']='mysql://username:password@Localhost/database_table_name
 app.config['SQLALCHEMY_DATABASE_URI']='mysql://root:@localhost/hms'
@@ -40,6 +48,10 @@ def patients():
 
 @app.route("/bookings")
 def bookings():
+    if  User.is_authenticated:
+        return render_template('bookings.html',username=current_user.username)
+    else:
+        return render_template('login.html')
     return render_template('bookings.html')
 
 @app.route("/signup",methods=['GET','POST'])
@@ -68,17 +80,17 @@ def login():
     if request.method=='POST':
         email=request.form.get('email')
         password=request.form.get('password')
-        print(email)
-    #     user=User.query.filter_by(email=email).first()
-    #     print
-    #     if user and check_password_hash(user.password,password):
-    #         login_user(user)
-    #         return redirect(url_for('/'))
+        user=User.query.filter_by(email=email).first()
+        if user and check_password_hash(user.password,password):
+            login_user(user)
+            return redirect(url_for("bookings"))
     return render_template('login.html')
 
 @app.route("/logout")
+@login_required
 def logout():
-    return render_template('login.html')
+    logout_user()
+    return redirect(url_for("login"))
 
 
 @app.route('/test')
